ac169528ad020f8475bd9bd60948b435bdc058ee
William Anderson <william.minnesota@gmail.com>
2016-10-18 20:04:36 -0500

    fixed a thingie.

--- ticket_system/JS/master.js
+++ ticket_system/JS/master.js
@@ -3,7 +3,7 @@ function ticketPrompt() {
 	var clss = Math.floor((Math.random() * 10000000) + 1);
 	clss == clss + '';
 	var ticketNum = window.prompt("Ticket number:", "146462");
-	var name = window.prompt("Name:", "Edwardo")
+	var name = window.prompt("Name:", "Edwardo");
 	var finalText = "Ticket Number: " +ticketNum+ " - Name: " + name; 
 	var prevText =document.getElementById("tickets").innerHTML;
 	document.getElementById("tickets").innerHTML = prevText + '<div id=' + clss +' class="black-border"> <p></p> <div>'+finalText+"</div><button id='check-in-btn' class='ui-button' onclick=checkIn(" + clss + ")>Check in</button>";
@@ -13,5 +13,13 @@ function ticketPrompt() {
 function checkIn(button_clss) {
 
 	var test = document.getElementById(button_clss).innerHTML = "<p></p>Checked In";
+
+}
+
+function seat_selector(seat_num) {
+
+	var ticketNum = window.prompt('Ticket number', '146462');
+	var name = window.prompt('Name', 'Edwardo')
+	document.getElementById(seat_num).innerHTML= "SEAT " + seat_num + " IS RESERVED BY " + name;
 
 }
