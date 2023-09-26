dba6b5b48edbd6685fb7b02c1267ac93569d25fd
DeadHamberger <william.minnesota@gmail.com>
2023-09-26 00:38:36 -0500

    Began new svelte page.
    Initialized Svelte app. Idea sketched on new design for website.

---
+++ new/dhpage/src/routes/+page.svelte
@@ -0,0 +1,129 @@
+<style>
+    .page-container {
+        background-color: rgb(232, 232, 246);
+        width: 100vw;
+        height: 100vh;
+        margin:0;
+        padding: 0;
+    }
+
+    .main-nav-button {
+        position: absolute;
+        background-color: inherit;
+        border: 0;
+        color: #28aae2;
+        font-size: 36pt;
+        font-weight: 700;
+        margin: 0px;
+        transition-duration: 175ms;
+        padding-bottom: 5px;
+        padding-left: 5px;
+        
+        border-top:10px solid transparent;
+        border-right:10px solid transparent;
+        border-bottom:10px solid transparent;
+        border-left:10px solid transparent;
+    }
+
+    .main-nav-button:hover {
+        color: black;
+        background-color: whitesmoke;
+        text-shadow: #001, 10px, 10px, 10px;
+        /* padding-right: 10px;
+        padding-bottom: 10px; */
+        padding-top: 5px;
+        padding-left: 10px;
+
+        border-top:10px solid black;
+        border-right:10px solid transparent;
+        border-bottom:10px solid transparent;
+        border-left:10px solid black;
+    }
+
+    #main-nav-btn-1 {
+        top: 72pt;
+        left: 18vh;
+    }
+    #main-nav-btn-2 {
+        top: 144pt;
+        left: 30vw;
+    }
+    #main-nav-btn-3 {
+        top: 252pt;
+        left: 15vw;
+    }
+
+    #background-words {
+        color:rgba(185, 185, 202, 0.714);
+        font-size: 18pt;
+        width: inherit;
+        overflow:hidden;
+        text-overflow: clip;
+        white-space: nowrap;
+        padding: 00px;
+        line-height: 18pt;
+    }
+
+    #background-words p {
+       margin: 0;
+    }
+
+    h1 {
+        position: absolute;
+        top: 30vh;
+        left: 45vw;
+        font-size: 84pt;
+        text-shadow: #001, 1px, 2px, 0px;
+    }
+
+    a {
+    color: inherit;
+    text-decoration: none;
+    }
+
+</style>
+
+<!-- <Navbar /> -->
+
+<div class="page-container">
+    <div id="background-words">
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+        <p>name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name name</p>
+    </div>
+
+    <button class="main-nav-button" id="main-nav-btn-1">
+        <a href="/experience">experience</a>
+    </button>
+
+    <button class="main-nav-button" id="main-nav-btn-2">
+        projects
+    </button>
+
+    <button class="main-nav-button" id="main-nav-btn-3">
+        current
+    </button>
+
+    <h1>William A</h1>
+
+</div>
---
+++ new/dhpage/src/routes/experience/+page.svelte
@@ -0,0 +1,5 @@
+<div>
+    <h1>Education</h1>
+    <h1>Work</h1>
+    <h1>Resume:</h1>
+</div>
